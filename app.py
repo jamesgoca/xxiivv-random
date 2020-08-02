67ef05081e2cf4a11aa56b0aed8be3235759255b
James Gallagher <jamesg@jamesg.app>
2020-08-02 10:33:53 +0100

    feat: Save links to CSV for efficiency

--- app.py
+++ app.py
@@ -2,23 +2,41 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import random
 import webbrowser
+import os
+import csv
 
 def main():
-	xxvvii_ring_page = "https://webring.xxiivv.com/"
+	if not os.path.isfile("links.csv"):
+		xxvvii_ring_page = "https://webring.xxiivv.com/"
 
-	page = urlopen(xxvvii_ring_page)
+		page = urlopen(xxvvii_ring_page)
 
-	soup = BeautifulSoup(page, "html.parser")
+		soup = BeautifulSoup(page, "html.parser")
 
-	names = soup.findAll("li")
+		names = soup.findAll("li")
 
-	links = [n.a for n in names]
+		links = [n.a for n in names]
 
-	random_link = random.choice(links)
+		print(links)
 
-	print("Opening {}".format(random_link.text))
+		with open("links.csv", "w+") as link_file:
+			filewriter = csv.writer(link_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+			for l in links:
+				filewriter.writerow([l["href"], l.text])
 
-	webbrowser.open(random_link["href"])
+	with open("links.csv", "r") as link_file:
+		filereader = csv.reader(link_file, delimiter=',')
+		final_links = []
+		for l in filereader:
+			final_links.append([l[0], l[1]])
+
+	random_link = random.choice(final_links)
+
+	print(random_link)
+
+	print("Opening {}".format(random_link[1]))
+
+	webbrowser.open(random_link[0])
 
 if __name__ == "__main__":
 	main()
